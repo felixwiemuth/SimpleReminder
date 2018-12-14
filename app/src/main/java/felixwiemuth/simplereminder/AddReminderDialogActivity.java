1a0451b8243ab1d3f4c87678b0bf7cbbbe61f3cb
Felix Wiemuth <felixwiemuth@hotmail.de>
2018-12-14 12:29:25 +0100

    Add empty AddReminderDialogActivity

---
+++ app/src/main/java/felixwiemuth/simplereminder/AddReminderDialogActivity.java
@@ -0,0 +1,13 @@
+package felixwiemuth.simplereminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddReminderDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder_dialog);
+    }
+}
