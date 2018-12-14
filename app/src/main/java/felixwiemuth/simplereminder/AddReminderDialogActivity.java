4396f2f4c17bc27a8e6a9f9a0ae77acb7b647ef6
Felix Wiemuth <felixwiemuth@hotmail.de>
2018-12-14 17:07:50 +0100

    Add basic UI for adding reminders

--- app/src/main/java/felixwiemuth/simplereminder/AddReminderDialogActivity.java
+++ app/src/main/java/felixwiemuth/simplereminder/AddReminderDialogActivity.java
@@ -1,7 +1,16 @@
 package felixwiemuth.simplereminder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class AddReminderDialogActivity extends AppCompatActivity {
 
@@ -9,5 +18,50 @@ public class AddReminderDialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder_dialog);
+
+        final AutoCompleteTextView nameTextView = findViewById(R.id.nameTextView);
+
+        final TimePicker timePicker = findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour;
+                int minute;
+                boolean tomorrow = false;
+               
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    hour = timePicker.getHour();
+                    minute = timePicker.getMinute();
+                } else {
+                    hour = timePicker.getCurrentHour();
+                    minute = timePicker.getCurrentMinute();
+                }
+
+                Calendar time = Calendar.getInstance();
+                time.set(Calendar.HOUR_OF_DAY, hour);
+                time.set(Calendar.MINUTE, minute);
+
+                if (time.compareTo(Calendar.getInstance()) <= 0) { // TODO might want to add margin if ReminderManager does not send out reminder when time has already elapsed
+                    time.add(Calendar.DAY_OF_MONTH, 1);  //TODO test: does it wrap over midnight?
+                    tomorrow = true;
+                }
+
+                ReminderManager.addReminder(AddReminderDialogActivity.this, time.getTimeInMillis(), nameTextView.getText().toString());
+                String text = getString(R.string.toast_reminder_added_for);
+                if (tomorrow) {
+                    text += getString(R.string.toast_reminder_added_tomorrow);
+                }
+                text += DateFormat.getTimeInstance(DateFormat.SHORT).format(time.getTime());
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(AddReminderDialogActivity.this, text, duration);
+                toast.show();
+
+                finish(); //TODO change animation
+            }
+        });
     }
 }
---
+++ app/src/main/java/felixwiemuth/simplereminder/ReminderManager.java
@@ -0,0 +1,28 @@
+package felixwiemuth.simplereminder;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import static android.content.Context.ALARM_SERVICE;
+
+/**
+ * Manages reminders by allowing to add and change reminders.
+ *
+ * @author Felix Wiemuth
+ */
+public class ReminderManager {
+    public static void addReminder(Context context, long time, String text) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        Intent processIntent = new Intent(context, ReminderService.class);
+        PendingIntent alarmIntent = PendingIntent.getService(context, 0, processIntent, 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, alarmIntent);
+        } else {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, time, alarmIntent);
+        }
+
+    }
+}
