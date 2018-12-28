0b7609dc6f14cf95b5b47412c719b3b79f43ddd6
Felix Wiemuth <felixwiemuth@hotmail.de>
2018-12-28 15:06:03 +0100

    Make editing of state preferences thread-safe

--- app/src/main/java/felixwiemuth/simplereminder/ReminderManager.java
+++ app/src/main/java/felixwiemuth/simplereminder/ReminderManager.java
@@ -30,9 +30,10 @@ import felixwiemuth.simplereminder.data.Reminder;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static android.content.Context.ALARM_SERVICE;
-import static android.content.Context.MODE_PRIVATE;
 import static felixwiemuth.simplereminder.SharedPrefs.PREF_STATE_CURRENT_REMINDERS;
 import static felixwiemuth.simplereminder.SharedPrefs.PREF_STATE_NEXTID;
 
@@ -42,42 +43,83 @@ import static felixwiemuth.simplereminder.SharedPrefs.PREF_STATE_NEXTID;
  * @author Felix Wiemuth
  */
 public class ReminderManager {
+    /**
+     * Lock guarding the state preferences ({@link SharedPrefs#PREFS_STATE}). This reference being null is equivalent to the lock not being aquired.
+     */
+    private static Lock prefStateLock;
+
+    private static void lock() {
+        if (prefStateLock == null) {
+            prefStateLock = new ReentrantLock();
+        }
+        prefStateLock.lock();
+    }
+
+    private static void unlock() {
+        if (prefStateLock == null) {
+            return;
+        }
+    }
+
+    interface StatePrefEditOperation {
+        void edit(SharedPreferences prefs, SharedPreferences.Editor editor);
+    }
+
+    /**
+     * Edit the state preferences ({@link SharedPrefs#PREFS_STATE}) exclusively and commit after the operation has successfully completed. This ensures that different threads editing these preferences do not overwrite their changes.
+     *
+     * @param context
+     * @param operation
+     */
+    @SuppressLint("ApplySharedPref")
+    private static void performExclusivelyOnStatePrefsAndCommit(Context context, StatePrefEditOperation operation) {
+        lock();
+        try {
+            SharedPreferences prefs = SharedPrefs.getStatePrefs(context);
+            SharedPreferences.Editor editor = prefs.edit();
+            operation.edit(prefs, editor);
+            editor.commit();
+        } finally {
+            unlock();
+        }
+    }
+
     @SuppressLint("ApplySharedPref")
     public static void addReminder(Context context, long date, String text) {
-        // Get next alarm ID
-        SharedPreferences prefs = context.getSharedPreferences(SharedPrefs.PREFS_STATE, MODE_PRIVATE);
-        final int nextId = prefs.getInt(PREF_STATE_NEXTID, 0);
-        Reminder reminder = new Reminder(nextId, date, text);
+        performExclusivelyOnStatePrefsAndCommit(context, (prefs, editor) -> {
 
-        List<Reminder> currentReminders = getRemindersFromPrefs(prefs);
-        List<Reminder> updatedReminders = new ArrayList<>(currentReminders);
-        updatedReminders.add(reminder);
+            // Get next alarm ID
+            final int nextId = prefs.getInt(PREF_STATE_NEXTID, 0);
+            Reminder reminder = new Reminder(nextId, date, text);
 
-        prefs.edit()
-                .putInt(PREF_STATE_NEXTID, nextId + 1)
-                .putString(PREF_STATE_CURRENT_REMINDERS, Reminder.toJson(updatedReminders))
-                .commit(); // use commit so that the ID really cannot be used again
+            List<Reminder> currentReminders = getRemindersFromPrefs(prefs);
+            List<Reminder> updatedReminders = new ArrayList<>(currentReminders);
+            updatedReminders.add(reminder);
 
+            editor
+                    .putInt(PREF_STATE_NEXTID, nextId + 1)
+                    .putString(PREF_STATE_CURRENT_REMINDERS, Reminder.toJson(updatedReminders));
 
-        // Prepare pending intent
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        Intent processIntent = new Intent(context, ReminderService.class);
-        processIntent
-                .putExtra(ReminderService.EXTRA_INT_ID, nextId)
-                .putExtra(ReminderService.EXTRA_STRING_REMINDER_TEXT, text);
-        PendingIntent alarmIntent = PendingIntent.getService(context, nextId, processIntent, 0);
+            // Prepare pending intent
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+            Intent processIntent = new Intent(context, ReminderService.class);
+            processIntent
+                    .putExtra(ReminderService.EXTRA_INT_ID, nextId)
+                    .putExtra(ReminderService.EXTRA_STRING_REMINDER_TEXT, text);
+            PendingIntent alarmIntent = PendingIntent.getService(context, nextId, processIntent, 0);
 
-        // Schedule alarm
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, date, alarmIntent);
-            Log.d("ReminderManager", "Set alarm (\"exact and allow while idle\") for " + DateFormat.getDateTimeInstance().format(date));
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, date, alarmIntent);
-            Log.d("ReminderManager", "Set alarm (\"exact\") for " + DateFormat.getDateTimeInstance().format(date));
-        } else {
-            alarmManager.set(AlarmManager.RTC_WAKEUP, date, alarmIntent);
-            Log.d("ReminderManager", "Set alarm for " + DateFormat.getDateTimeInstance().format(date));
-        }
+            // Schedule alarm
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, date, alarmIntent);
+                Log.d("ReminderManager", "Set alarm (\"exact and allow while idle\") for " + DateFormat.getDateTimeInstance().format(date));
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, date, alarmIntent);
+                Log.d("ReminderManager", "Set alarm (\"exact\") for " + DateFormat.getDateTimeInstance().format(date));
+            } else {
+                alarmManager.set(AlarmManager.RTC_WAKEUP, date, alarmIntent);
+                Log.d("ReminderManager", "Set alarm for " + DateFormat.getDateTimeInstance().format(date));
+            }
+        });
     }
 
     public static List<Reminder> getReminders(Context context) {
