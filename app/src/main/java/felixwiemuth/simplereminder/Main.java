b185741cd330dc959269e9940aefcd4ca9ac40e6
Felix Wiemuth <felixwiemuth@hotmail.de>
2019-03-09 14:44:52 +0100

    Activate ACRA

--- app/src/main/java/felixwiemuth/simplereminder/Main.java
+++ app/src/main/java/felixwiemuth/simplereminder/Main.java
@@ -18,15 +18,35 @@
 package felixwiemuth.simplereminder;
 
 import android.app.Application;
+import android.content.Context;
 import androidx.preference.PreferenceManager;
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraDialog;
+import org.acra.annotation.AcraMailSender;
 
 /**
  * @author Felix Wiemuth
  */
+@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraMailSender(mailTo = "felixwiemuth@hotmail.de", reportAsFile = false)
+@AcraDialog(
+        resTitle = R.string.acra_title,
+        resText = R.string.acra_prompt,
+        resCommentPrompt = R.string.acra_comment_prompt)
 public class Main extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (ACRA.isACRASenderServiceProcess()) { // If ACRA started the application for a crash report, do nothing
+            return;
+        }
         PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this);
     }
 }
