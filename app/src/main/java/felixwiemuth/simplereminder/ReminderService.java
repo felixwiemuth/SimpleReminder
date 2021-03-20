felixwiemuth/SimpleReminder

commit 666944223f10e32b7af21aa9354990ffdd91a12b

Add action to ReminderService intents to prevent loss of extras

app/src/main/java/felixwiemuth/simplereminder/ReminderService.java
@@ -26,6 +26,10 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import java.util.Arrays;
+import java.util.Date;
+
 import felixwiemuth.simplereminder.data.Reminder;
 import felixwiemuth.simplereminder.ui.AddReminderDialogActivity;
 import felixwiemuth.simplereminder.ui.reminderslist.RemindersListActivity;
@@ -42,6 +46,7 @@ import lombok.Builder;
 public class ReminderService extends IntentService {
     public static final String CHANNEL_REMINDER = "Reminder";
     public static final String EXTRA_INT_ID = "felixwiemuth.simplereminder.ReminderService.extra.ID";
+    public static final String ACTION_START = "felixwiemuth.simplereminder.ReminderService.action.START";
 
     private static Uri defaultSound;
 
@@ -80,6 +85,9 @@ public class ReminderService extends IntentService {
                 }
 
                 intent.putExtra(ReminderService.EXTRA_INT_ID, id);
+                // Note: Setting an action seems to prevent extras being removed from intents, see https://stackoverflow.com/questions/15343840/intent-extras-missing-when-activity-started.
+                // It happened that the service was called without extras after introducing editing of reminders.
+                intent.setAction(ACTION_START);
                 EnumUtil.serialize(action).to(intent);
 
                 return intent;
@@ -121,6 +129,7 @@ public class ReminderService extends IntentService {
      * @return
      */
     public static PendingIntent getCancelNotifyIntent(Context context, int id) {
+        // Note: This intent is only used to be passed to AlarmManager.cancel(...), so it shouldn't start the service.
         return PendingIntent.getService(context, id, new Intent(context, ReminderService.class), 0); // must use equal intent and same request code as when scheduled
     }
 
@@ -167,6 +176,9 @@ public class ReminderService extends IntentService {
         if (intent == null) {
             Log.w("ReminderService", "Service called with no intent.");
             return;
+        }
+        if (! intent.hasExtra(EXTRA_INT_ID)) {
+            throw new IllegalArgumentException("ReminderService called without reminder ID extra.");
         }
         int id = intent.getExtras().getInt(EXTRA_INT_ID, -1);
         Action action = EnumUtil.deserialize(Action.class).from(intent);

commit f54eb991987c1e599e50c46d830ebe3185af992d

Fix incorrect rescheduling of reminders leading to crash
Commit 666944223f10e32b7af21aa9354990ffdd91a12b introduced an
action added to intents scheduled with AlarmManager to prevent
loss of extras. Actually the loss of extras was probably due
to the reuse of old pending intents when rescheduling. Using
the action when scheduling but not when cancelling actually
caused the pending intents not to be cancelled correctly;
but as a side-effect, the intent used to cancel could not be
mistakenly reused either.

After cancelling was corrected in 7a3c121526d04b2f1f1a13123d4724fa49cc3210
by adding the action also to the intent used to cancel,
intents could be confused again and probably the intent
used to cancel was sometimes reused when rescheduling
reminders which led to crashes as ReminderService would
throw an exception when called without extras (which the
intent used to cancel does not have and need).

This is now fixed by using PendingIntent.FLAG_CANCEL_CURRENT
for each pending intent to start ReminderService.

app/src/main/java/felixwiemuth/simplereminder/ReminderService.java
@@ -93,6 +93,8 @@ public class ReminderService extends IntentService {
                 intent.putExtra(ReminderService.EXTRA_INT_ID, id);
                 // Note: Setting an action seems to prevent extras being removed from intents, see https://stackoverflow.com/questions/15343840/intent-extras-missing-when-activity-started.
                 // It happened that the service was called without extras after introducing editing of reminders.
+                // This might, however, also have happened due to accidental reuse of intents when rescheduling a reminder
+                // (the intent used to cancel the original alarm could have been considered equal and reuesed for the new alarm).
                 intent.setAction(ACTION_START);
                 EnumUtil.serialize(action).to(intent);
 
@@ -101,6 +103,7 @@ public class ReminderService extends IntentService {
 
             /**
              * Create a pending intent that will start the service. Sets correct request code.
+             * Uses flag {@link PendingIntent#FLAG_CANCEL_CURRENT} to make sure no old intent is reused.
              *
              * @param context
              * @return
@@ -118,7 +121,7 @@ public class ReminderService extends IntentService {
                     default:
                         throw new ImplementationError("Unknown action.");
                 }
-                return PendingIntent.getService(context, requestCode, build(context), 0);
+                return PendingIntent.getService(context, requestCode, build(context), PendingIntent.FLAG_CANCEL_CURRENT);
             }
         }
     }
@@ -136,9 +139,10 @@ public class ReminderService extends IntentService {
      */
     public static PendingIntent getCancelAlarmNotifyIntent(Context context, int id) {
         // Note: This intent is only used to be passed to AlarmManager.cancel(...), so it shouldn't start the service.
-        // It must be equal to the intent used when scheduling the alarm.
+        // It must be equal to the intent used when scheduling the alarm (extras are not compared)
         Intent intent = new Intent(context, ReminderService.class);
         intent.setAction(ACTION_START);
+        // Note that it shouldn't matter whether this reuses an intent that matches; it is enough that it matches to cancel the alarm.
         return PendingIntent.getService(context, id, intent, 0); // Request code must be the same as when scheduled.
     }
 